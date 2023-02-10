1860fb335ad6c8fae99d0524b47c85c763331f7e
Ammar Bandukwala <ammar@ammar.io>
2023-02-09 18:38:32 -0600

    Split up app

---
+++ pages/[...path].tsx
@@ -0,0 +1,5 @@
+import Home from ".";
+
+export default function ViewNote() {
+  return <>505</>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,21 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import createTheme from "@mui/material/styles/createTheme";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>s.cr — encrypted, disposable notes</title>
+        <meta
+          name="description"
+          content="Securely send encrypted, self-destructing notes."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>{" "}
       <Head>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
         <meta
@@ -19,7 +30,37 @@ export default function App({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
       </Head>
-      <Component {...pageProps} />
+      <div
+        className="site-container h-screen w-screen flex flex-col md:container sm:px-12 md:px-120 md:mx-auto p-3 md:py-8 lg:px-100"
+        style={{
+          maxWidth: "700px",
+        }}
+      >
+        <span className="frontmatter">
+          <div className="flex items-center mb-2">
+            <Link href="/">
+              <Image
+                width="48"
+                height="48"
+                alt="Icon"
+                src={"/favicon.ico"}
+              ></Image>
+            </Link>
+            <div className="ml-3">
+              <h1>s.cr</h1>
+              <p>Send encrypted, disposable notes</p>
+            </div>
+          </div>
+          <p className="tagline mt-3">
+            s.cr encrypts your note in your browser, so our server has no
+            information about its contents. All notes expire after a period of
+            time, defaulting to 24 hours.
+          </p>
+        </span>
+
+        <hr style={{ marginTop: "12px", marginBottom: "16px" }} />
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
