7fb75bfd988e02508829d7ee6209d6e693bd2f9c
Ammar Bandukwala <ammar@ammar.io>
2024-09-11 16:47:56 -0500

    Reduce max file size limit
    Lower the max file size to 90MB to stay within Cloudflare's 100MB
    free tier limit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,8 @@ interface FileInputProps {
   onFileChange: (file: File | null) => void;
 }
 
-const maxFileSize = 2e8;
+// maxFileSize is 90MB because cloudflare has a 100MB free limit.
+const maxFileSize = 90e6;
 
 // maxUploadDuration returns the maximum duration in hours for a file upload based on its size.
 const maxUploadDuration = (size: number): number => {
